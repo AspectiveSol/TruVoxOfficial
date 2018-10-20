36d4454b0642528440061fd2bd6fecff40bde198
PatGomz <patricknangkilgomez@gmail.com>
2018-10-21 03:43:20 +0800

    index updates
    integration of body post navigations

--- index.php
+++ index.php
@@ -6,3 +6,38 @@
 */
 
 get_header();?>
+
+	<?php do_action('TruVox-AS_before_content'); ?>
+
+	<div id="primary" class="content-area col-md-9">
+		<main id="main" class="post-wrap" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php do_action('TruVox-AS_after_content'); ?>
+
+<?php get_footer();?>
